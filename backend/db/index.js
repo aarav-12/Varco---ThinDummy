bb66ef5fb29fb46dfe12944dccad9788a033d8fc
aaravmithrani <aaravmithrani@ailifebot.in>
2026-04-02 20:26:13 +0530

    added postgres url

--- backend/db/index.js
+++ backend/db/index.js
@@ -1,13 +1,11 @@
 /* eslint-disable no-undef */
 const { Pool } = require("pg");
-require("dotenv").config();
 
 const pool = new Pool({
-  user: "postgres",
-  host: "localhost",
-  database: "health_mvp",
-  password: "mithrani0000",
-  port: 5432,
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
 
 module.exports = pool;
